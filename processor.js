c91bb398896f8a58bdb9ef15a9cf329b28f0d5f5
averagehat <bigmwp@gmail.com>
2019-10-15 16:08:26 -0400

    still working. added rectangle. alpha?

--- processor.js
+++ processor.js
@@ -1,64 +1,5 @@
-
-function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min) ) + min;
-}
-
-const randChoice = (arr, default_) => 
-    (arr.length == 0) ? default_ : arr[randInt(0, arr.length)]
-
-const CoOrd = (xy) => { x: xy[0], y: xy[1] };
-interface Cell<T> {
-    data?: T
-}
-const directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 0), (0, 1), (1, -1), (1, 0), (1, 1)].map(CoOrd);
-function getMove(pos, board) { 
-    const possibleMoves = directions.map(xy => 
-        (pos.x + xy.x, pos.y + xy.y)
-    ); 
-    const validMoves = possibleMoves.filter(xy =>
-        board[xy.x][xy.y].data == undefined
-    );
-    const move = randChoice(validMoves, (0, 0))
-    return move
-}
-
-const performMove = (pos, board) => {
-    move = getMove(pos, board); 
-    let newBoard = board.map(board => board.slice()); // copy
-    const newData = board[ pos.x ][pos.y].data + 1;
-    newBoard[ move.x ] [ move.y ] = { data: newData };
-    return newBoard; 
-}
-
-// could use distance from the center instead of age
-// want to compute max-dist actually
-const oDist = (board, cell) => (board.origin.x - cell.x)^2 + (board.origin.y - cell.y)^2
-const computeColors = (board, cell) => { 
-    const ALPHA = 1
-    const dists = populated.map(c => oDist(board, c))
-    const maxDist = (Math.max(...dists));
-    const charred = (maxDist < 2) ? 1 :  1 - (oDist(cell) / maxDist);
-    return [255*charred,255*charred,255*charred, ALPHA]; 
-}
-const newImg = (frame, board) => {
-  const populated = board.entries().flatMap(e =>
-      e[1].map(e2 => {
-          const [x, y, cell] = [e[0], e2[0], e2[1]];
-          return cell.data ? {x: x, y: y, data: cell.data} : null
-      })).filter(id)
-  populated.map(cell => {
-    const red = cell.y * (width * 4) + cell.x * 4;
-    const colNalpha = computeColors(board, cell);
-    // [redVal, greenVal, blueVal, alphaVal] = computeColors(board, cell);
-    for (let i = 0; i < colNalpha.length; i++) {
-      frame[red+i] = colNalpha[i]; 
- }
-  });
-  return frame;
-  }
-
-
-
+// have to instantiate board based on image size and width
+//
 let processor = {
     timerCallback: function() {
       if (this.video.paused || this.video.ended) {
@@ -82,13 +23,14 @@ let processor = {
       this.c2 = document.getElementById("c2");
       this.ctx2 = this.c2.getContext("2d");
       let self = this;
-      let cx = self.video.videoWidth / 4;
-      let cy = self.video.videoHeight / 4;
-      let game.origin = 
+//      let cx = self.video.videoWidth / 4;
+//      let cy = self.video.videoHeight / 4;
       this.video.addEventListener("play", function() {
           self.width = self.video.videoWidth / 2;
           self.height = self.video.videoHeight / 2;
           self.timerCallback();
+//          self.center = (self.width / 2, self.height / 2);
+//          console.log('center', center);
         }, false);
     },
   
@@ -96,8 +38,7 @@ let processor = {
       // this.ctx1.canvas.crossOrigin = "Anonymous";
       this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
       let frame = this.ctx1.getImageData(0, 0, this.width, this.height);
-      let l = frame.data.length / 4;
-  
+      let l = frame.data.length / 4; 
       for (let i = 0; i < l; i++) {
         let r = frame.data[i * 4 + 0];
         let g = frame.data[i * 4 + 1];
@@ -106,7 +47,11 @@ let processor = {
           frame.data[i * 4 + 1] = 0;
           frame.data[i * 4 + 2] = 0;
       }
-      this.ctx2.putImageData(frame, 0, 0);
+      this.ctx2.putImageData(frame, 0, 0); 
+      let newData = this.ctx1.getImageData(this.width / 2, this.height / 2 , 30, 30);
+      for (let i = 0; i < newData.length; i++) { newData[i] = 255; }
+      
+      this.ctx2.putImageData(newData, this.width / 2, this.height / 2);
       return;
     }
   };
