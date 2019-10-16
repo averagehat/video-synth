afdf765d3b739dad7877b1929ecd3e7bf3f4ef07
averagehat <bigmwp@gmail.com>
2019-10-16 01:48:02 -0400

    midi working

--- osc-server/index.js
+++ osc-server/index.js
@@ -10,8 +10,14 @@ const app = express();
 app.use(express.static(path.join(__dirname, '/public')));
 
 const server = createServer(app);
+const easymidi = require('easymidi'); 
+const T = require("timbre");
+
 const wss = new WebSocket.Server({ server,     autoAcceptConnections: true });
 let queue = [];
+
+const midi2freq =  m => 2**((m-69)/12)*440
+
 wss.on('connection', function(ws) {
     const id = setInterval( () => {
         let next;
@@ -21,6 +27,33 @@ wss.on('connection', function(ws) {
             ws.send(next, console.log);
         }
     }, 100);
+
+  let midiOn = (easymidi.getInputs().length > 0)
+  console.log('midi available', midiOn, easymidi.getInputs());
+  let midiInput = new easymidi.Input(easymidi.getInputs()[0]);
+  let synth = T("OscGen", {wave:"saw", mul:0.25}).play();
+  const playNoteOn = m => synth.noteOnWithFreq(midi2freq(m), 100);
+  midiInput.on('noteon', midi => {
+    console.log('MIDI message received', Date.now(), midi);
+    const MAX = 255;
+    let rgb = [0, 0, 0];
+    // Midi: note, channel, velocity, _type ('noteon')
+    if (midi.note < 51) rgb[0] = MAX;
+    else if ( (midi.note >= 51) && (midi.note < 61)) rgb[1] = MAX;
+    else rgb[2] = MAX;
+    const data = {...midi, time: Date.now(), color: rgb};
+    playNoteOn(midi.note);
+    const VISUAL_DELAY = 700;
+    setTimeout(() => 
+       wss.clients.forEach(function each(client) {
+         if (client.readyState === WebSocket.OPEN) {
+           client.send( JSON.stringify(data) );
+         }
+       }), VISUAL_DELAY);
+  });
+
+
+
    // ws.send(JSON.stringify({time : 0.5, color : [0, 255, 0, 1]}));
 //  const id = setInterval(function() {
 //    ws.send(JSON.stringify(process.memoryUsage()), function() {
@@ -46,6 +79,7 @@ wss.on('connection', function(ws) {
   ws.on('close', function() {
     console.log('stopping client interval');
     clearInterval(id);
+    midiInput.close()
   });
 });
 
